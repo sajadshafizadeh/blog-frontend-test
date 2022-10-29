0862e9f6f106ed3433fc8a37f9a3f73a60741dc6
Sajad Shafizadeh <vip.saj.71@gmail.com>
2022-10-29 14:34:17 +0330

    Getting list of posts from API

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,10 +1,47 @@
 import {PostItems} from '../../components/PostItems';
 
-const PostsList = () => {
+const PostsList = (props) => {
 	
+	console.log(props.posts);
+
 	return (
-		<PostItems />
+		<div>
+			<PostItems />
+			<span>check the console</span>
+		</div>
 	);
 }
 
+
+export async function getServerSideProps (context){
+
+	// To define posts service provider 
+	// const postsURL = 'https://jsonplaceholder.typicode.com/posts'; -- Not supported in Iran
+	const postsURL = 'https://dummyjson.com/posts';
+
+	try {
+
+		// To get the posts & convert it to json formatterd
+		const posts = await fetch(postsURL).then( (res) => res.json() );
+
+		// To return props
+		return {
+			props: {
+				posts,
+			},
+		};
+
+	// To do error handling
+	} catch (error) {
+		return {
+			props: {
+				posts: [],
+				error: error,
+			},
+		};
+	}
+
+}
+
+// To set PostsList method as the default executable one
 export default PostsList;
